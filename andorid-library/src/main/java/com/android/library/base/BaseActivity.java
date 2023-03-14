d80d2a15358d6bef0c2c2b9f6b247f8a3b3120a9
tongxiaobo <tongxiaobo@imydao.com>
2023-03-14 17:45:36 +0800

    依赖优化

--- andorid-library/src/main/java/com/android/library/base/BaseActivity.java
+++ andorid-library/src/main/java/com/android/library/base/BaseActivity.java
@@ -3,7 +3,6 @@ package com.android.library.base;
 import android.os.Bundle;
 
 import com.android.library.manager.AppManager;
-import com.blankj.utilcode.util.ActivityUtils;
 import com.qmuiteam.qmui.arch.QMUIFragmentActivity;
 
 public class BaseActivity extends QMUIFragmentActivity {
--- app/src/main/java/com/android/library/MainActivity.java
+++ app/src/main/java/com/android/library/MainActivity.java
@@ -1,15 +1,27 @@
 package com.android.library;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.android.library.base.BaseActivity;
 
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import rxhttp.wrapper.param.RxHttp;
 
 
 public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//
+         RxHttp.get("http://www.baidu.com")
+                       .toObservableString()
+                       .observeOn(AndroidSchedulers.mainThread())
+                       .subscribe(s -> {
+                }, throwable  -> {
+                    throwable.printStackTrace();
+                    //Log.i(TAG,"ssss"+throwable.toString());
+                   Toast.makeText(MainActivity.this,throwable.toString(),Toast.LENGTH_LONG).show();
+                });
     }
 }

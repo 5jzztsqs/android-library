d2efe5bf65cf97cbf711f9f7634b831fa5f1109f
tongxiaobo <tongxiaobo@imydao.com>
2023-03-15 15:13:30 +0800

    图片加载添加

--- andorid-library/src/main/java/com/android/library/BaseApplication.java
+++ andorid-library/src/main/java/com/android/library/BaseApplication.java
@@ -3,12 +3,14 @@ package com.android.library;
 import android.app.Application;
 
 import com.android.library.manager.MMKVManger;
+import com.blankj.utilcode.util.Utils;
 import com.qmuiteam.qmui.arch.QMUISwipeBackActivityManager;
 
 public abstract class BaseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Utils.init(this);
         MMKVManger.init(this);
         QMUISwipeBackActivityManager.init(this);
     }

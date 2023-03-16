3718e134516472e59e0382c0dbc49ed9d6cb384d
pcq <tongxiaobo@imydao.com>
2023-03-16 17:31:30 +0800

    ////

--- andorid-library/src/main/java/com/android/library/base/BaseFragment.java
+++ andorid-library/src/main/java/com/android/library/base/BaseFragment.java
@@ -1,13 +1,14 @@
 package com.android.library.base;
 
-import android.app.Activity;
 import android.content.Context;
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
 import com.qmuiteam.qmui.arch.QMUIFragment;
 
 public abstract class BaseFragment extends QMUIFragment {
 
-    protected Activity activity;
+    protected FragmentActivity activity;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
